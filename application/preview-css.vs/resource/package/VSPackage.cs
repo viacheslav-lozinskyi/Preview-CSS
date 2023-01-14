430c5e88367f241b468b4675dea513b4f3fdc1e8
Viacheslav Lozinskyi <viacheslav.lozinskyi@gmail.com>
2023-01-14 13:21:19 +0200

    - Minor improvements

--- application/preview-css.vs/resource/package/VSPackage.cs
+++ application/preview-css.vs/resource/package/VSPackage.cs
@@ -756,7 +756,7 @@ namespace resource.package
             public const string DESCRIPTION = "Quick preview of CSS files";
             public const string GUID = "C6915272-78B1-4B1E-A31C-00BACBE2A500";
             public const string NAME = "Preview-CSS";
-            public const string VERSION = "1.0.2";
+            public const string VERSION = "1.0.1";
         }
 
         private Events s_Events = null;
