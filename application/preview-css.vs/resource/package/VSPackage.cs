ff080196bf9b0fc504e93071b0fd18478733fd94
Viacheslav Lozinskyi <viacheslav.lozinskyi@gmail.com>
2023-06-19 07:25:57 +0300

    - Adaptation to new version of MetaOutput.Client (3.0.0)

--- application/preview-css.vs/resource/package/VSPackage.cs
+++ application/preview-css.vs/resource/package/VSPackage.cs
@@ -1,13 +1,11 @@
 ﻿
 using EnvDTE;
-using EnvDTE80;
 using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
 //using Microsoft.VisualStudio.Workspace;
 //using Microsoft.VisualStudio.Workspace.VSIntegration.Contracts;
 using System;
-using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Threading;
 using Task = System.Threading.Tasks.Task;
@@ -768,45 +766,9 @@ namespace resource.package
 
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
-            {
-                extension.AnyPreview.Connect();
-                extension.AnyPreview.Register(".CSS", new resource.preview.VSPreview());
-            }
-            {
-                await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
-            }
-            try
-            {
-                if (string.IsNullOrEmpty(atom.Trace.GetFailState(CONSTANT.APPLICATION)) == false)
-                {
-                    var a_Context = Package.GetGlobalService(typeof(SDTE)) as DTE2;
-                    if (a_Context != null)
-                    {
-                        var a_Context1 = (OutputWindowPane)null;
-                        for (var i = a_Context.ToolWindows.OutputWindow.OutputWindowPanes.Count; i >= 1; i--)
-                        {
-                            if (a_Context.ToolWindows.OutputWindow.OutputWindowPanes.Item(i).Name == CONSTANT.HOST)
-                            {
-                                a_Context1 = a_Context.ToolWindows.OutputWindow.OutputWindowPanes.Item(i);
-                                break;
-                            }
-                        }
-                        if (a_Context1 == null)
-                        {
-                            a_Context1 = a_Context.ToolWindows.OutputWindow.OutputWindowPanes.Add(CONSTANT.HOST);
-                        }
-                        if (a_Context1 != null)
-                        {
-                            a_Context1.OutputString("\r\n" + CONSTANT.NAME + " extension doesn't work without MetaOutput.\r\n    Please install it --> https://www.metaoutput.net/download\r\n");
-                            a_Context1.Activate();
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex.ToString());
-            }
+            extension.AnyPreview.Connect(CONSTANT.APPLICATION, CONSTANT.NAME);
+            extension.AnyPreview.Register(".CSS", new resource.preview.VSPreview());
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
             {
                 Package s_Package = this;
                 //var a_ServiceProvider = this;
@@ -874,10 +836,8 @@ namespace resource.package
 
         protected override int QueryClose(out bool canClose)
         {
-            {
-                extension.AnyPreview.Disconnect();
-                canClose = true;
-            }
+            extension.AnyPreview.Disconnect();
+            canClose = true;
             return VSConstants.S_OK;
         }
     }
